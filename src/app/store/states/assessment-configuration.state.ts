b9f51e3dfcae1bb57899a2c01e7d6775ece570c6
chingaloEric <echingalo@gmail.com>
2019-08-17 14:13:20 +0300

    fix: updated the app state with the use of entities
    used the ngrx/entities in the state of the configurations

--- src/app/store/states/assessment-configuration.state.ts
+++ src/app/store/states/assessment-configuration.state.ts
@@ -1,15 +1,43 @@
 import { BaseState, initialBaseState } from './base.state';
 import { AssessmentConfiguration } from 'src/app/pages/configuration/models/assessment-configuration.model';
+import { EntityState, EntityAdapter, createEntityAdapter } from '@ngrx/entity';
 
-export interface AssessmentConfigurationState extends BaseState {
-  assessmentConfig: AssessmentConfiguration[];
+export interface AssessmentConfigurationState
+  extends BaseState,
+    EntityState<AssessmentConfiguration> {
   updating: boolean;
   updated: boolean;
 }
 
-export const initialConfigurationState: AssessmentConfigurationState = {
-  ...initialBaseState,
-  assessmentConfig: [],
-  updated: false,
-  updating: false
-};
+export function sortByIndicator(
+  config1: AssessmentConfiguration,
+  config2: AssessmentConfiguration
+): number {
+  return config1.indicator.localeCompare(config2.indicator);
+}
+
+export function selectConfigId(config: AssessmentConfiguration): string {
+  return config.id;
+}
+
+export const adapter: EntityAdapter<
+  AssessmentConfiguration
+> = createEntityAdapter({
+  sortComparer: sortByIndicator,
+  selectId: selectConfigId
+});
+
+export const initialConfigurationState: AssessmentConfigurationState = adapter.getInitialState(
+  {
+    ...initialBaseState,
+    updated: false,
+    updating: false
+  }
+);
+
+export const {
+  selectIds: seletcAssessmentIds,
+  selectEntities: selectAssessmentConfigEntities,
+  selectAll: selectAllAssessmentConfigurations,
+  selectTotal: selectAssessmntConfigCount
+} = adapter.getSelectors();
--- src/app/store/states/verification-configuration.state.ts
+++ src/app/store/states/verification-configuration.state.ts
@@ -1,15 +1,41 @@
 import { BaseState, initialBaseState } from './base.state';
 import { VerificationConfiguration } from 'src/app/pages/configuration/models/verification-configuration.model';
+import { EntityState, EntityAdapter, createEntityAdapter } from '@ngrx/entity';
 
-export interface VerificationConfigurationState extends BaseState {
-  verificationConfig: VerificationConfiguration[];
+export interface VerificationConfigurationState
+  extends BaseState,
+    EntityState<VerificationConfiguration> {
   updating: boolean;
   updated: boolean;
 }
 
-export const initialConfigurationState: VerificationConfigurationState = {
-  ...initialBaseState,
-  verificationConfig: [],
-  updated: false,
-  updating: false
-};
+export function sortByIndicator(
+  config1: VerificationConfiguration,
+  config2: VerificationConfiguration
+): number {
+  return config1.indicator.localeCompare(config2.indicator);
+}
+
+export function selectConfigId(config: VerificationConfiguration): string {
+  return config.id;
+}
+
+export const adapter: EntityAdapter<
+  VerificationConfiguration
+> = createEntityAdapter({
+  selectId: selectConfigId,
+  sortComparer: sortByIndicator
+});
+export const initialConfigurationState: VerificationConfigurationState = adapter.getInitialState(
+  {
+    ...initialBaseState,
+    updated: false,
+    updating: false
+  }
+);
+export const {
+  selectIds: selectVerificationConfigIds,
+  selectEntities: selectVerificationConfigEntities,
+  selectAll: selectAllVerificationConfigurations,
+  selectTotal: selectVerificationConfigCount
+} = adapter.getSelectors();
