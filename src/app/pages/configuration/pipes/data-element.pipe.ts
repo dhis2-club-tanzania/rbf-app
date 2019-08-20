c701ae6899676e512bbaa56889b3451fd279a50a
chingaloEric <echingalo@gmail.com>
2019-08-20 14:49:45 +0300

    fix: displayed data element names instead of ids

--- src/app/pages/configuration/pipes/data-element.pipe.ts
+++ src/app/pages/configuration/pipes/data-element.pipe.ts
@@ -1,7 +1,9 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { DataElementList } from '../models/data-element.model';
 import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
+import * as _ from 'lodash';
+
+import { DataElementList } from '../models/data-element.model';
 import { State } from 'src/app/store/reducers';
 import { getAllDataElements } from 'src/app/store/selectors';
 
@@ -13,7 +15,14 @@ export class DataElementPipe implements PipeTransform {
   constructor(private store: Store<State>) {
     this.dataElements = this.store.select(getAllDataElements);
   }
+  dataElementList = [];
+
   transform(value?: string): any {
-    return value;
+    this.dataElements.subscribe(data => (this.dataElementList = data));
+    const dataElement: DataElementList = _.find(
+      this.dataElementList,
+      (data: DataElementList) => data.id === value
+    );
+    return dataElement.name;
   }
 }
