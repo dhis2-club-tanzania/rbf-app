9bcc80d43e0d0689272e94e68b222683a59117be
Maswi MR <mrblack360@github.com>
2019-08-20 08:17:40 +0300

    refactor: Added OrgUnit Filter

--- src/app/pages/assessment/assessment.module.ts
+++ src/app/pages/assessment/assessment.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AssessmentComponent } from './components/assessment/assessment.component';
 import { RoutingModule } from 'src/app//pages/assessment/assessment.routes';
+import { NgxDhis2OrgUnitFilterModule } from '@iapps/ngx-dhis2-org-unit-filter';
 
 @NgModule({
   declarations: [AssessmentComponent],
   imports: [
     CommonModule,
-    RoutingModule
+    RoutingModule,
+    NgxDhis2OrgUnitFilterModule
   ]
 })
 export class AssessmentModule { }
