f366f6fe18d5e828aa8fa90de67c4dbae5754d2a
chingaloEric <echingalo@gmail.com>
2019-08-20 05:32:35 +0300

    feat: added error message on configurations not available

--- src/app/pages/configuration/components/assessment-list/assessment-list.component.ts
+++ src/app/pages/configuration/components/assessment-list/assessment-list.component.ts
@@ -3,7 +3,11 @@ import { Observable } from 'rxjs';
 import { AssessmentConfiguration } from '../../models/assessment-configuration.model';
 import { Store } from '@ngrx/store';
 import { State } from 'src/app/store/reducers';
-import { getAssessmentConfigurations } from 'src/app/store/selectors';
+import {
+  getAssessmentConfigurations,
+  getAssessmentConfigErrorState
+} from 'src/app/store/selectors';
+import { ErrorMessage } from 'src/app/core';
 
 @Component({
   selector: 'app-assessment-list',
@@ -12,10 +16,14 @@ import { getAssessmentConfigurations } from 'src/app/store/selectors';
 })
 export class AssessmentListComponent implements OnInit {
   assessmentIndicators$: Observable<AssessmentConfiguration[]>;
+  assessmentConfigurationError$: Observable<ErrorMessage>;
   constructor(private store: Store<State>) {}
 
   ngOnInit() {
     this.assessmentIndicators$ = this.store.select(getAssessmentConfigurations);
+    this.assessmentConfigurationError$ = this.store.select(
+      getAssessmentConfigErrorState
+    );
   }
 }
 export interface AssessmentIndicators {
--- src/app/pages/configuration/components/verification-list/verification-list.component.ts
+++ src/app/pages/configuration/components/verification-list/verification-list.component.ts
@@ -3,7 +3,11 @@ import { Observable } from 'rxjs';
 import { VerificationConfiguration } from '../../models/verification-configuration.model';
 import { Store } from '@ngrx/store';
 import { State } from 'src/app/store/reducers';
-import { getVerificationConfigurations } from 'src/app/store/selectors';
+import {
+  getVerificationConfigurations,
+  getVerificationConfigErrorState
+} from 'src/app/store/selectors';
+import { ErrorMessage } from 'src/app/core';
 
 @Component({
   selector: 'app-verification-list',
@@ -12,11 +16,15 @@ import { getVerificationConfigurations } from 'src/app/store/selectors';
 })
 export class VerificationListComponent implements OnInit {
   verificationIndicators$: Observable<VerificationConfiguration[]>;
+  verificationConfigurationError$: Observable<ErrorMessage>;
   constructor(private store: Store<State>) {}
 
   ngOnInit() {
     this.verificationIndicators$ = this.store.select(
       getVerificationConfigurations
+    );
+    this.verificationConfigurationError$ = this.store.select(
+      getVerificationConfigErrorState
     );
   }
 }
