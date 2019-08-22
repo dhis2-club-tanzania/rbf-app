f3b90e3868ba6f85848724866facddbbe9fdc529
Maswi MR <mrblack360@github.com>
2019-08-22 15:40:44 +0300

    refactor: Added Assessment Form

--- src/app/pages/assessment/components/assessment/assessment.component.ts
+++ src/app/pages/assessment/components/assessment/assessment.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { PeriodFilterConfig } from '@iapps/ngx-dhis2-period-filter';
+import { Observable } from 'rxjs';
+import { AssessmentConfiguration  } from '../../../configuration/models/assessment-configuration.model';
+import { State } from 'src/app/store/reducers';
+import { Store } from '@ngrx/store';
+import { getAssessmentConfigurations } from 'src/app/store/selectors';
 
 @Component({
   selector: 'app-assessment',
@@ -7,6 +12,8 @@ import { PeriodFilterConfig } from '@iapps/ngx-dhis2-period-filter';
   styleUrls: ['./assessment.component.css']
 })
 export class AssessmentComponent implements OnInit {
+
+  assessmentIndicators$: Observable<AssessmentConfiguration[]>;
 
   orgUnitObject: any;
   periodObject: any;
@@ -25,9 +32,11 @@ export class AssessmentComponent implements OnInit {
   selectedOrgUnitItems: any[] = [];
   selectedPeriodItems: any[] = [];
 
-  constructor() { }
+  constructor(private store: Store<State>) { }
 
-  ngOnInit() {  }
+  ngOnInit() {
+    this.assessmentIndicators$ = this.store.select(getAssessmentConfigurations);
+   }
 
   onOrgUnitUpdate(orgUnitObject, action) {
     this.orgUnitObject = orgUnitObject;
