95a46589d4ea11b7b61fd191ed5a01e9a5acb452
chingaloEric <echingalo@gmail.com>
2019-08-26 01:16:10 +0300

    feat: added the general configuration reducer

--- src/app/store/reducers/general-configuration.reducers.ts
+++ src/app/store/reducers/general-configuration.reducers.ts
@@ -1,43 +1,75 @@
-// import { createReducer, on } from '@ngrx/store';
-// import { initialGeneralConfigurationState } from '../states/general-configuration.state';
-// import {
-//   loadGeneralConfigurations,
-//   loadGeneralConfigurationsFail,
-//   loadGeneralConfigurationsSucess,
-//   addGeneralConfigurations,
-//   addGeneralConfigurationsFail,
-//   addGeneralConfigurationsSuccess
-// } from '../actions/general-config.actions';
-// import {
-//   loadingBaseState,
-//   loadedBaseState,
-//   errorBaseState
-// } from '../states/base.state';
-// import { adapter } from '../states/general-configuration.state';
+import { createReducer, on } from '@ngrx/store';
+import {
+  initialGeneralConfigurationState,
+  GeneralConfigurationState
+} from '../states/general-configuration.state';
+import {
+  loadGeneralConfigurations,
+  loadGeneralConfigurationsFail,
+  loadGeneralConfigurationsSucess,
+  addGeneralConfigurations,
+  addGeneralConfigurationsFail,
+  addGeneralConfigurationsSuccess,
+  updateGeneralConfigurations,
+  updateGeneralConfigurationsFail,
+  updateGeneralConfigurationsSuccess
+} from '../actions/general-config.actions';
+import {
+  loadingBaseState,
+  loadedBaseState,
+  errorBaseState
+} from '../states/base.state';
+import { adapter } from '../states/general-configuration.state';
 
-// export const reducer = createReducer(
-//   initialGeneralConfigurationState,
-//   on(loadGeneralConfigurations, state => ({ ...state, loadingBaseState })),
-//   on(loadGeneralConfigurationsFail, (state, { error }) => ({
-//     ...state,
-//     ...errorBaseState,
-//     error
-//   })),
-//   on(loadGeneralConfigurationsSucess, (state, { configurations }) =>
-//     adapter.addOne(configurations, { ...state, ...loadedBaseState })
-//   ),
-//   on(addGeneralConfigurations, state => ({
-//     ...state,
-//     added: false,
-//     adding: true
-//   })),
-//   on(addGeneralConfigurationsFail, (state, { error }) => ({
-//     ...state,
-//     ...errorBaseState,
-//     adding: false,
-//     error
-//   })),
-//   on(addGeneralConfigurationsSuccess, (state, { configuration }) =>
-//     adapter.addOne(configuration, { ...state, adding: false, added: true })
-//   ), on()
-// );
+export const reducer = createReducer(
+  initialGeneralConfigurationState,
+  on(loadGeneralConfigurations, state => ({ ...state, loadingBaseState })),
+  on(loadGeneralConfigurationsFail, (state, { error }) => ({
+    ...state,
+    ...errorBaseState,
+    error
+  })),
+  on(loadGeneralConfigurationsSucess, (state, { configurations }) =>
+    adapter.addOne(configurations, { ...state, ...loadedBaseState })
+  ),
+  on(addGeneralConfigurations, state => ({
+    ...state,
+    added: false,
+    adding: true
+  })),
+  on(addGeneralConfigurationsFail, (state, { error }) => ({
+    ...state,
+    ...errorBaseState,
+    adding: false,
+    error
+  })),
+  on(addGeneralConfigurationsSuccess, (state, { configuration }) =>
+    adapter.addOne(configuration, { ...state, adding: false, added: true })
+  ),
+  on(updateGeneralConfigurations, state => ({
+    ...state,
+    updated: false,
+    updating: true
+  })),
+  on(updateGeneralConfigurationsFail, (state, { error }) => ({
+    ...state,
+    updated: false,
+    updating: false,
+    ...errorBaseState,
+    error
+  })),
+  on(updateGeneralConfigurationsSuccess, (state, { configuration }) =>
+    adapter.updateOne(configuration, {
+      ...state,
+      updated: true,
+      updating: false
+    })
+  )
+);
+
+export function generalConfigurationReducer(
+  state,
+  action
+): GeneralConfigurationState {
+  return reducer(state, action);
+}
