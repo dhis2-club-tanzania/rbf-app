277f73779136e6a2774f7b4a4f8fd6fde2199d35
Maswi MR <mrblack360@github.com>
2019-08-27 17:00:18 +0300

    refactor(Report and Verification Modules): Added Selection Filters
    The selection filters replaces the functions that were executed by the Periodic Filter and
    Organisation Unit Filter

--- src/app/pages/verification/components/verification/verification.component.ts
+++ src/app/pages/verification/components/verification/verification.component.ts
@@ -1,6 +1,8 @@
 import { PeriodFilterConfig } from '@iapps/ngx-dhis2-period-filter';
 import { Component, OnInit } from '@angular/core';
 import { OrgUnitFilterConfig } from '@iapps/ngx-dhis2-org-unit-filter';
+import { SelectionFilterConfig } from '@iapps/ngx-dhis2-selection-filters';
+import { AngularWaitBarrier } from 'blocking-proxy/built/lib/angular_wait_barrier';
 
 @Component({
   selector: 'app-verification',
@@ -8,30 +10,28 @@ import { OrgUnitFilterConfig } from '@iapps/ngx-dhis2-org-unit-filter';
   styleUrls: ['./verification.component.css']
 })
 export class VerificationComponent implements OnInit {
-  showOrgUnitFilter = false;
-  showPeriodFilter = false;
+  dataSelections: any;
+  showForm = false;
+  selectionFilterConfig: SelectionFilterConfig = {
+    allowStepSelection: true,
+    showDynamicDimension: false,
+    showDataFilter: false,
+    showValidationRuleGroupFilter: false,
+    stepSelections: ['pe', 'ou', 'dx', 'vrg'],
+    dataFilterConfig: {
+      singleSelection: true,
+      enabledSelections: []
+    },
+    periodFilterConfig: {
+      singleSelection: true
+    }
+  };
 
-  periodObject: any;
   orgUnitObject: any;
-  periodFilterConfig: PeriodFilterConfig = {
-    singleSelection: true,
-    emitOnSelection: false
-  };
-  action: string;
-  orgUnitFilterConfig: OrgUnitFilterConfig = {
-    singleSelection: false,
-    showUserOrgUnitSection: false,
-    showOrgUnitLevelGroupSection: false,
-    showOrgUnitGroupSection: true,
-    showOrgUnitLevelSection: false
-  };
-  selectedOrgUnitItems: any[] = [];
-  selectedPeriodItems: any[] = [];
-
-  orgUnitLevel: any;
-
+  periodObject: any;
+  orgUnitLevel: string;
+  action;
   periodLooper = [];
-  testLooper = '';
 
   // Form Properties are deckared below
 
@@ -39,16 +39,15 @@ export class VerificationComponent implements OnInit {
 
   ngOnInit() {}
 
-  onOrgUnitToggle() {
-    this.showOrgUnitFilter = !this.showOrgUnitFilter;
-  }
-
-  onPeriodToggle() {
-    this.showPeriodFilter = !this.showPeriodFilter;
+  onFilterUpdateAction(dataSelections) {
+    this.dataSelections = dataSelections;
+    this.setShowForm();
+    this.setOrgUnitLevel();
+    this.setPeriodLooper();
   }
 
   setOrgUnitLevel() {
-    switch (this.orgUnitObject.items[0].level) {
+    switch (this.dataSelections[1].items[0].level) {
       case 1:
         this.orgUnitLevel = 'National';
         break;
@@ -63,127 +62,166 @@ export class VerificationComponent implements OnInit {
         break;
     }
   }
-
-  onOrgUnitUpdate(orgUnitObject, action) {
-    this.orgUnitObject = orgUnitObject;
-    this.action = action;
-    this.onOrgUnitToggle();
-    this.setOrgUnitLevel();
-  }
-  onPeriodUpdate(periodObject, action) {
-    this.periodObject = periodObject;
-    this.action = action;
-    this.onPeriodToggle();
-    this.setPeriodLooper();
-    console.log(periodObject);
-  }
-
   setPeriodLooper() {
-    if (this.periodObject.items[0].type === 'Monthly') {
-      this.periodLooper = [this.periodObject.items[0].name];
+    if (this.dataSelections[0].items[0].type === 'Monthly') {
+      this.periodLooper = [this.dataSelections[0].items[0].name];
     }
-    if (this.periodObject.items[0].type === 'BiMonthly') {
-      this.testLooper = this.periodObject.items[0];
-      switch (this.periodObject.items[0].id.charAt(5)) {
+    if (this.dataSelections[0].items[0].type === 'BiMonthly') {
+      switch (this.dataSelections[0].items[0].id.charAt(5)) {
         case '1':
           {
-            this.periodLooper = ['January', 'February'];
+            this.periodLooper = [
+              'January '.concat(this.dataSelections[0].items[0].id.slice(0, 4)),
+              'February '.concat(this.dataSelections[0].items[0].id.slice(0, 4))
+            ];
           }
           break;
         case '2':
           {
-            this.periodLooper = ['March', 'April'];
+            this.periodLooper = [
+              'March '.concat(this.dataSelections[0].items[0].id.slice(0, 4)),
+              'April '.concat(this.dataSelections[0].items[0].id.slice(0, 4))
+            ];
           }
           break;
         case '3':
           {
-            this.periodLooper = ['May', 'June'];
+            this.periodLooper = [
+              'May '.concat(this.dataSelections[0].items[0].id.slice(0, 4)),
+              'June '.concat(this.dataSelections[0].items[0].id.slice(0, 4))
+            ];
           }
           break;
         case '4':
           {
-            this.periodLooper = ['July', 'August'];
+            this.periodLooper = [
+              'July '.concat(this.dataSelections[0].items[0].id.slice(0, 4)),
+              'August '.concat(this.dataSelections[0].items[0].id.slice(0, 4))
+            ];
           }
           break;
         case '5':
           {
-            this.periodLooper = ['September', 'October'];
+            this.periodLooper = [
+              'September '.concat(
+                this.dataSelections[0].items[0].id.slice(0, 4)
+              ),
+              'October '.concat(this.dataSelections[0].items[0].id.slice(0, 4))
+            ];
           }
           break;
         case '6':
           {
-            this.periodLooper = ['November', 'December'];
+            this.periodLooper = [
+              'November '.concat(
+                this.dataSelections[0].items[0].id.slice(0, 4)
+              ),
+              'December '.concat(this.dataSelections[0].items[0].id.slice(0, 4))
+            ];
           }
           break;
       }
     }
-    if (this.periodObject.items[0].type === 'Quarterly') {
-      switch (this.periodObject.items[0].id.charAt(5)) {
+    if (this.dataSelections[0].items[0].type === 'Quarterly') {
+      switch (this.dataSelections[0].items[0].id.charAt(5)) {
         case '1':
           {
-            this.periodLooper = ['January', 'February', 'March'];
+            this.periodLooper = [
+              'January '.concat(this.dataSelections[0].items[0].id.slice(0, 4)),
+              'February '.concat(
+                this.dataSelections[0].items[0].id.slice(0, 4)
+              ),
+              'March '.concat(this.dataSelections[0].items[0].id.slice(0, 4))
+            ];
           }
           break;
         case '2':
           {
-            this.periodLooper = ['April', 'May', 'June'];
+            this.periodLooper = [
+              'April '.concat(this.dataSelections[0].items[0].id.slice(0, 4)),
+              'May '.concat(this.dataSelections[0].items[0].id.slice(0, 4)),
+              'June '.concat(this.dataSelections[0].items[0].id.slice(0, 4))
+            ];
           }
           break;
         case '3':
           {
-            this.periodLooper = ['July', 'August', 'September'];
+            this.periodLooper = [
+              'July '.concat(this.dataSelections[0].items[0].id.slice(0, 4)),
+              'August '.concat(this.dataSelections[0].items[0].id.slice(0, 4)),
+              'September '.concat(
+                this.dataSelections[0].items[0].id.slice(0, 4)
+              )
+            ];
           }
           break;
         case '4':
           {
-            this.periodLooper = ['October', 'November', 'December'];
+            this.periodLooper = [
+              'October '.concat(this.dataSelections[0].items[0].id.slice(0, 4)),
+              'November '.concat(
+                this.dataSelections[0].items[0].id.slice(0, 4)
+              ),
+              'December '.concat(this.dataSelections[0].items[0].id.slice(0, 4))
+            ];
           }
           break;
       }
     }
-    if (this.periodObject.items[0].type === 'SixMonthly') {
-      switch (this.periodObject.items[0].id.charAt(5)) {
+    if (this.dataSelections[0].items[0].type === 'SixMonthly') {
+      switch (this.dataSelections[0].items[0].id.charAt(5)) {
         case '1':
           {
             this.periodLooper = [
-              'January',
-              'February',
-              'March',
-              'April',
-              'May',
-              'June'
+              'January '.concat(this.dataSelections[0].items[0].id.slice(0, 4)),
+              'February '.concat(
+                this.dataSelections[0].items[0].id.slice(0, 4)
+              ),
+              'March '.concat(this.dataSelections[0].items[0].id.slice(0, 4)),
+              'April '.concat(this.dataSelections[0].items[0].id.slice(0, 4)),
+              'May '.concat(this.dataSelections[0].items[0].id.slice(0, 4)),
+              'June '.concat(this.dataSelections[0].items[0].id.slice(0, 4))
             ];
           }
           break;
         case '2':
           {
             this.periodLooper = [
-              'July',
-              'August',
-              'September',
-              'October',
-              'November',
-              'December'
+              'July '.concat(this.dataSelections[0].items[0].id.slice(0, 4)),
+              'August '.concat(this.dataSelections[0].items[0].id.slice(0, 4)),
+              'September '.concat(
+                this.dataSelections[0].items[0].id.slice(0, 4)
+              ),
+              'October '.concat(this.dataSelections[0].items[0].id.slice(0, 4)),
+              'November '.concat(
+                this.dataSelections[0].items[0].id.slice(0, 4)
+              ),
+              'December '.concat(this.dataSelections[0].items[0].id.slice(0, 4))
             ];
           }
           break;
       }
     }
-    if (this.periodObject.items[0].type === 'Yearly') {
+    if (this.dataSelections[0].items[0].type === 'Yearly') {
       this.periodLooper = [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-        'July',
-        'August',
-        'September',
-        'October',
-        'November',
-        'December'
+        'January '.concat(this.dataSelections[0].items[0].id.slice(0, 4)),
+        'February '.concat(this.dataSelections[0].items[0].id.slice(0, 4)),
+        'March '.concat(this.dataSelections[0].items[0].id.slice(0, 4)),
+        'April '.concat(this.dataSelections[0].items[0].id.slice(0, 4)),
+        'May '.concat(this.dataSelections[0].items[0].id.slice(0, 4)),
+        'June '.concat(this.dataSelections[0].items[0].id.slice(0, 4)),
+        'July '.concat(this.dataSelections[0].items[0].id.slice(0, 4)),
+        'August '.concat(this.dataSelections[0].items[0].id.slice(0, 4)),
+        'September '.concat(this.dataSelections[0].items[0].id.slice(0, 4)),
+        'October '.concat(this.dataSelections[0].items[0].id.slice(0, 4)),
+        'November '.concat(this.dataSelections[0].items[0].id.slice(0, 4)),
+        'December '.concat(this.dataSelections[0].items[0].id.slice(0, 4))
       ];
+    }
+  }
+  setShowForm() {
+    if (this.dataSelections[1].items[0].level) {
+      this.showForm = true;
     }
   }
 }
